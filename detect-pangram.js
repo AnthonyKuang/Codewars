4dd0e218529354b86641447a40e2227463fa3a43
Anthony Kuang <kuanganthony@gmail.com>
2020-07-23 06:06:26 -0700

    .every solution

--- detect-pangram.js
+++ detect-pangram.js
@@ -1,7 +1,11 @@
-function isPangram(string) {
-  const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+// .every returns a boolean if every value satisfies the given condition
 
-  return alphabet.map(letter => string.split(' ').join('').toLowerCase().split('').includes(letter)).includes(false) ? false : true;
+function isPangram(string) {
+  // const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+  // return alphabet.map(letter => string.split(' ').join('').toLowerCase().split('').includes(letter)).includes(false) ? false : true;
+
+  return 'abcdefghijklmnopqrstuvwxyz'.split('').every(letter => string.toLowerCase().indexOf(letter) !== -1);
 }
 
-console.log(isPangram('This is not a panagram.'));
+console.log(isPangram('A quick brown fox jumps over the lazy dog'));
