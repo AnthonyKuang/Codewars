8acce6af7fd153fd3874ce784b196f9fde792b14
Anthony Kuang <kuanganthony@gmail.com>
2020-07-26 11:00:21 -0700

    hella nice solution

--- number-expanded-form.js
+++ number-expanded-form.js
@@ -1,20 +1,30 @@
+// function expandedForm(num) {
+//   const numIndex = num.toString().split('').reverse();
+//   let array = [];
+//   numIndex.forEach((digit, index) => {
+//     let numberZero = '';
+//     for (i = index; i > 0; i--) {
+//       numberZero += '0';
+//     }
+//     array.push(digit + numberZero);
+//   })
+//   let answerString = '';
+//   array.reverse().forEach(num => {
+//     if (parseInt(num) !== 0) {
+//       answerString += `${num} + `;
+//     }
+//   })
+//   return answerString.slice(0, -3);
+// }
+
 function expandedForm(num) {
-  const numIndex = num.toString().split('').reverse();
-  let array = [];
-  numIndex.forEach((digit, index) => {
-    let numberZero = '';
-    for (i = index; i > 0; i--) {
-      numberZero += '0';
-    }
-    array.push(digit + numberZero);
-  })
-  let answerString = '';
-  array.reverse().forEach(num => {
-    if (parseInt(num) !== 0) {
-      answerString += `${num} + `;
-    }
-  })
-  return answerString.slice(0, -3);
+  return String(num)
+        .split("")
+        .reverse()
+        .map((digit, index) => digit * Math.pow(10, index))
+        .filter(a => a > 0)
+        .reverse()
+        .join(" + ");
 }
 
 console.log(expandedForm(70304));
